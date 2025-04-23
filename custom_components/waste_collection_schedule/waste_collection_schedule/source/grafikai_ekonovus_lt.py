1c483baea240d40acf0560a8f7141fcbb0b94162
Pandosas <pandosas@gmail.com>
2025-04-23 17:13:15 +0300

    ekonovus

---
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/grafikai_ekonovus_lt.py
@@ -0,0 +1,84 @@
+import json
+from datetime import datetime
+
+import requests
+from waste_collection_schedule import Collection  # type: ignore[attr-defined]
+
+TITLE = "Ekonovus"
+DESCRIPTION = 'Source for UAB "Ekonovus".'
+URL = "https://grafikai.svara.lt"
+TEST_CASES = {
+    "Demokratų g. 7, Kaunas": {
+        "region": "Kauno m. sav.",
+        "street": "Demokratų g.",
+        "house_number": "7",
+        "waste_object_ids": [101358, 100858, 100860],
+    },
+    "Alytaus g. 2, Išlaužo k., Išlaužo sen. Prienų r. sav.": {
+        "region": "Prienų r. sav.",
+        "street": "Alytaus g.",
+        "house_number": "2",
+        "district": "Išlaužo sen.",
+    },
+}
+
+ICON_MAP = {
+    "mišrių atliekų": "mdi:trash-can",
+    "antrinių žaliavų (popierius/plastikas)": "mdi:recycle",
+    "antrinių žaliavų (stiklas)": "mdi:glass-fragile",
+    "žaliųjų atliekų": "mdi:leaf",
+}
+
+class Source:
+    API_URL = "https://wabi-west-europe-d-primary-api.analysis.windows.net/"
+
+    def __init__(
+        self, region, street, house_number, district=None, waste_object_ids=None
+    ):
+        if waste_object_ids is None:
+            waste_object_ids = []
+        self._region = region
+        self._street = street
+        self._house_number = house_number
+        self._waste_object_ids = waste_object_ids
+        self._district = district
+
+    def fetch(self):
+        r = requests.post(
+            self.API_URL + "public/reports/querydata?synchronous=true",
+            headers={
+                "accept": "application/json, text/plain, */*",
+                "accept-language": "en-US,en-GB;q=0.9,en;q=0.8",
+                "activityid": "87d287ea-3a5f-e45b-d040-132389997968",
+                "content-type": "application/json;charset=UTF-8",
+                "requestid": "09ea070f-c80f-0c81-c88a-6a1c2f7c22a3",
+                "sec-ch-ua": "\"Google Chrome\";v=\"135\", \"Not-A.Brand\";v=\"8\", \"Chromium\";v=\"135\"",
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": "\"Windows\"",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "cross-site",
+                "x-powerbi-resourcekey": "d86dc3d4-e915-4460-b12e-c925d3ae6c75"
+            },
+            data={"version":"1.0.0","queries":[{"Query":{"Commands":[{"SemanticQueryDataShapeCommand":{"Query":{"Version":2,"From":[{"Name":"s","Entity":"ScheduleDates","Type":0},{"Name":"w","Entity":"WasteObject","Type":0},{"Name":"a","Entity":"AllAddresses","Type":0},{"Name":"t","Entity":"Teritorijos konteinerių tvarkaraščiams","Type":0},{"Name":"s1","Entity":"Schedule","Type":0}],"Select":[{"Measure":{"Expression":{"SourceRef":{"Source":"s"}},"Property":"Datos"},"Name":"ScheduleDates.Datos"}],"Where":[{"Condition":{"In":{"Expressions":[{"Column":{"Expression":{"SourceRef":{"Source":"w"}},"Property":"Adresas"}}],"Values":[[{"Literal":{"Value":"'Miriniškių k. Laurų g. 12-1'"}}]]}}},{"Condition":{"In":{"Expressions":[{"Column":{"Expression":{"SourceRef":{"Source":"w"}},"Property":"Inventorinis nr."}}],"Values":[[{"Literal":{"Value":"'52-P-41136 (Pakuotė)'"}}]]}}},{"Condition":{"In":{"Expressions":[{"Column":{"Expression":{"SourceRef":{"Source":"a"}},"Property":"District"}}],"Values":[[{"Literal":{"Value":"'Kauno r. sav.'"}}]]}}},{"Condition":{"In":{"Expressions":[{"Column":{"Expression":{"SourceRef":{"Source":"s"}},"Property":"Future"}}],"Values":[[{"Literal":{"Value":"'true'"}}]]}}},{"Condition":{"In":{"Expressions":[{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Rodomas tvarkaraštis"}}],"Values":[[{"Literal":{"Value":"'1'"}}]]}}},{"Condition":{"In":{"Expressions":[{"Column":{"Expression":{"SourceRef":{"Source":"s"}},"Property":"OverNextRun"}}],"Values":[[{"Literal":{"Value":"true"}}]]}}},{"Condition":{"Not":{"Expression":{"In":{"Expressions":[{"Column":{"Expression":{"SourceRef":{"Source":"s1"}},"Property":"ScheduleId"}}],"Values":[[{"Literal":{"Value":"7127L"}}],[{"Literal":{"Value":"7128L"}}],[{"Literal":{"Value":"7129L"}}],[{"Literal":{"Value":"7131L"}}],[{"Literal":{"Value":"7132L"}}],[{"Literal":{"Value":"7133L"}}],[{"Literal":{"Value":"7134L"}}],[{"Literal":{"Value":"7135L"}}],[{"Literal":{"Value":"7136L"}}],[{"Literal":{"Value":"7137L"}}],[{"Literal":{"Value":"7138L"}}],[{"Literal":{"Value":"7139L"}}],[{"Literal":{"Value":"7140L"}}],[{"Literal":{"Value":"7141L"}}]]}}}}},{"Condition":{"And":{"Left":{"Not":{"Expression":{"Contains":{"Left":{"Column":{"Expression":{"SourceRef":{"Source":"w"}},"Property":"Inventorinis nr."}},"Right":{"Literal":{"Value":"'siuk'"}}}}}},"Right":{"Not":{"Expression":{"Contains":{"Left":{"Column":{"Expression":{"SourceRef":{"Source":"w"}},"Property":"Inventorinis nr."}},"Right":{"Literal":{"Value":"'šiuk'"}}}}}}}}}]},"Binding":{"Primary":{"Groupings":[{"Projections":[0]}]},"DataReduction":{"DataVolume":3,"Primary":{"Top":{}}},"Version":1},"ExecutionMetricsKind":1}}]},"QueryId":"","ApplicationContext":{"DatasetId":"90015897-045e-4f68-8f83-c40d1fc3bfc2","Sources":[{"ReportId":"06fc8043-9afa-43d6-88cc-3a1e73aaf964","VisualId":"161c6ed98b10564c91b7"}]}}],"cancelQueries":[],"modelId":1026609}
+        )
+
+        data = json.loads(r.text)
+
+        entries = []
+
+        entriesString = data["results"][0]["result"]["data"]["dsr"]["DS"][0]["PH"][0]["DM0"][0]["M0"]
+        dateEntries = entriesString.split(", ")
+
+        for collection_waste_object in dateEntries:
+            entries.append(
+                Collection(
+                    date=datetime.strptime(
+                        collection_waste_object["date"], "%Y-%m-%dT%H:%M:%S"
+                    ).date(),
+                    t="antrinių žaliavų (popierius/plastikas)",
+                    icon="mdi:recycle",
+                )
+            )
+
+        return entries
